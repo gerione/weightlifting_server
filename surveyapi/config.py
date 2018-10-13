bc4139bfdd5b9d68d54f5623386bb411d9111ab1
Gerald Kettlgruber <kettlgruber@gmail.com>
2018-10-13 17:19:57 +0200

    Config update
    Update config to automatically check for DATABASE_URL.

--- surveyapi/config.py
+++ surveyapi/config.py
@@ -3,9 +3,13 @@ config.py
 - settings for the flask application object
 """
 
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class BaseConfig(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///survey.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # used for encryption and session management
     SECRET_KEY = 'mysecretkey'
