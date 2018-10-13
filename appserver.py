b1bf3d6cca56d26e62044a1b6beeac68dbf707c9
Gerald Kettlgruber <kettlgruber@gmail.com>
2018-10-13 16:11:08 +0200

    Update appserver to support heroku

--- appserver.py
+++ appserver.py
@@ -2,8 +2,9 @@
 appserver.py
 - creates an application instance and runs the dev server
 """
+from surveyapi.application import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
-    from surveyapi.application import create_app
-    app = create_app()
     app.run(host='0.0.0.0')
